6a12450752af5b3d063a9b0b356a72f1613c9d1e
bcondict <jesus.junc10@gmail.com>
2024-09-05 13:22:31 -0500

    update readme imgage

--- frontend/README.md
+++ frontend/README.md
@@ -3,6 +3,7 @@
 This project was generated with create-next-app.
 
 ## Project Structure
+
 $PROJECT_ROOT
 | # all the files related to the frontend are in this folder
 |--> src
@@ -19,8 +20,14 @@ $PROJECT_ROOT
 |--> public
 
 ## Install and run
+
 ```bash
 npm install
 npm run dev
 ```
 
+## Images
+
+### Principal view
+
+![principal image](./ReadmeImages/First_screen.png)
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,45 +1,57 @@
-'use client';
-import styles from '@/styles/components/Sidebar.module.css'
-import { useState, useEffect } from 'react'
+"use client";
+import styles from "@/styles/components/Sidebar.module.css";
+import { useState, useEffect } from "react";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import {Home, ChevronRight, ChevronLeft, Public, Person, Favorite} from '@material-ui/icons';
-import { useAuth } from '@/context/authContext'
-
+import Image from "next/image";
+import Link from "next/link";
+import {
+  Home,
+  ChevronRight,
+  ChevronLeft,
+  Public,
+  Person,
+  Favorite,
+} from "@material-ui/icons";
+import { useAuth } from "@/context/authContext";
 
 const sidebarItems = [
   {
-    name: 'Home',
-    href: '/',
-    icon: Home
+    name: "Home",
+    href: "/",
+    icon: Home,
   },
   {
-    name: 'Saved Queries',
-    href: '/savedQueries',
-    icon: Favorite 
+    name: "Saved Queries",
+    href: "/savedQueries",
+    icon: Favorite,
   },
   {
-    name: 'Public Queries',
-    href: '/publicQueries',
-    icon: Public 
+    name: "Public Queries",
+    href: "/publicQueries",
+    icon: Public,
   },
-]
+];
 
 const Sidebar = ({ className, ...props }) => {
   const { user } = useAuth();
-  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   const toggleSidebar = () => {
-    setIsCollapsed(!isCollapsed)
-  }
-
+    setIsCollapsed(!isCollapsed);
+  };
 
   return (
-    <div className={styles.sidebarWrapper} >
+    <div className={styles.sidebarWrapper}>
       {/* Button */}
-      <button className={`${styles.sidebarButton} ${isCollapsed ? styles.collapsed : ''}`} onClick={toggleSidebar}>
-        {isCollapsed ? <ChevronRight className={styles.arrow}/> : <ChevronLeft className={styles.arrow}/>}
+      <button
+        className={`${styles.sidebarButton} ${isCollapsed ? styles.collapsed : ""}`}
+        onClick={toggleSidebar}
+      >
+        {isCollapsed ? (
+          <ChevronRight className={styles.arrow} />
+        ) : (
+          <ChevronLeft className={styles.arrow} />
+        )}
       </button>
 
       {/* Sidebar */}
@@ -57,36 +69,36 @@ const Sidebar = ({ className, ...props }) => {
         </div>
         {/* elements */}
         <ul className={styles.sidebarList}>
-          {sidebarItems.map(({name, href, icon: Icon}, index) => {
+          {sidebarItems.map(({ name, href, icon: Icon }, index) => {
             return (
               <Link key={index} href={href}>
                 <li className={styles.sidebarItem}>
-                  <Icon className={styles.sidebarIcon}/>
+                  <Icon className={styles.sidebarIcon} />
                   <span className={styles.sidebarName}>{name}</span>
                 </li>
               </Link>
-            )
+            );
           })}
         </ul>
 
         {user ? (
           <Link className={styles.user} href="/profile">
             <button className={styles.userButton}>
-              <Person className={styles.userIcon}/>
+              <Person className={styles.userIcon} />
               <span className={styles.userName}>{user.userName}</span>
             </button>
           </Link>
         ) : (
-          <Link  className={styles.user} href="/login">
+          <Link className={styles.user} href="/login">
             <button className={styles.userButton}>
-              <Person className={styles.userIcon}/>
+              <Person className={styles.userIcon} />
               <span className={styles.userName}>Sign In</span>
             </button>
           </Link>
         )}
       </aside>
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar;
